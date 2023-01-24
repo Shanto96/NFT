ee916026dfa319841be0e9d8da65ea91707e39a3
kibria <shan2neo@gmail.com>
2023-01-24 20:35:06 +0600

    -done responsiveness of tabs

--- src/Components/Roadmap/Roadmap.jsx
+++ src/Components/Roadmap/Roadmap.jsx
@@ -22,11 +22,13 @@ function Roadmap() {
         </span>
       </div>
       <div className="event-container d-flex">
-        <div className="left-events">
+        <div className="left-events relative">
+          <div className="d-tab left-bar absolute"></div>
           <EventInfo /> <EventInfo /> <EventInfo />
         </div>
         <div className="center-line"></div>
-        <div className="right-events">
+        <div className="right-events relative">
+          <div className="d-tab right-bar absolute"></div>
           <EventInfo right /> <EventInfo right /> <EventInfo right />
         </div>
       </div>
